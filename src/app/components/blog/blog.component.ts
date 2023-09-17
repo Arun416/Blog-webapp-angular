39de2e07ca2e8063cfb43e512b4bfbcb0e8c8d2c
ArunKarthikeyan <56011519+Arun416@users.noreply.github.com>
2023-09-17 11:42:57 +0000

    Update 3 files of best practices

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BlogService } from 'src/app/services/blog.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent implements OnInit {
-   blogs:any[] = [1,2,3,4,5,6]
-   blogsList:any;
-    constructor(private blogService:BlogService,private router:Router){}
-    ngOnInit(): void {
-      this.blogService.getBlogs().subscribe(resp=> this.blogsList = resp)
-    }
+export class BlogComponent implements OnInit, OnDestroy {
+  // blogs: any[] = [1, 2, 3, 4, 5, 6];
+  private blogSubscription!: Subscription;
+  public blogList: any;
+  constructor(
+    private blogService: BlogService,
+    private router: Router,
+  ) {}
+  ngOnInit(): void {
+    this.blogSubscription = this.blogService
+      .getBlogs()
+      .subscribe((resp) => (this.blogList = resp));
+  }
 
-    onPreview(id:any) {
-      this.router.navigate([id+'/preview'])
-    }
+  ngOnDestroy(): void {
+    this.blogSubscription.unsubscribe();
+  }
+
+  public myTrackingfn(index: number, value: any) {
+    return value;
+  }
+
+  onPreview(id: any) {
+    this.router.navigate([id + '/preview']);
+  }
 }
